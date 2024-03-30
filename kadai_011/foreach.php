9476af0995624e59b053bc2b279464cdf0684d34
Y7Murata <156630740+Y7Murata@users.noreply.github.com>
2024-03-30 13:58:05 +0900

    Update foreach.php

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -1,6 +1,6 @@
 <p>
          <?php
-         $user_names = ['名前' => '玉ねぎ', '価格' => 200, '産地' => '北海道'];
+         $user_names = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
 
          //  配列$user_namesのインデックスと値を1つずつ順番に出力する
          foreach ($user_names as $index => $value) {
